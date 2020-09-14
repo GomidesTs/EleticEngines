99c0d419ac3a28bf04c09333ec8bb6a63152025e
GomidesTs <gomidestulio5@gmail.com>
2020-09-14 00:04:23 -0300

    Creating employees models

--- eletric_engines/employees/models.py
+++ eletric_engines/employees/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Employyes(models.Model):
+    officce = models.CharField(max_length=150)
+    shedule = models.CharField(max_length=10)
+    salary = models.FloatField()
+    company_time = models.FloatField()
+    contract = models.CharField(max_length=150)
+
+    def __str__(self):
+        return str(self.id)
