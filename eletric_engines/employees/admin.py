97c2ef595983c7b577407a715f53a7f8dce79e54
GomidesTs <gomidestulio5@gmail.com>
2020-09-14 00:09:22 -0300

    Adding functionality to the employee administrator

--- eletric_engines/employees/admin.py
+++ eletric_engines/employees/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Employyes
 
 # Register your models here.
+
+@admin.register(Employyes)
+class Employyes_admim(admin.ModelAdmin):
+    list_display = ['id', 'officce', 'shedule', 'salary', 'company_time', 'contract']
