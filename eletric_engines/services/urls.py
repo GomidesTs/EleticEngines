0f5e2a28e54c52aed253a62e56e983ac4297f5af
GomidesTs <gomidestulio5@gmail.com>
2020-09-25 03:24:58 -0300

    Issues #1 Routes, insrting urls for services integration

--- eletric_engines/services/urls.py
+++ eletric_engines/services/urls.py
@@ -1,6 +1,14 @@
-from django.contrib.auth import views as auth_views
 from django.urls import path
+from django.contrib.auth import views as auth_views
+from .views import Services_index, Parts_index
+from .views import Services_record
+from .views import Services_update
+from .views import Services_delete
 
 urlpatterns = [
-    
+    path('services_index/', Services_index.as_view(), name='services_index'),
+    path('services_record/', Services_record.as_view(), name='services_record'),
+    path('services_update/<int:pk>', Services_update.as_view(), name='services_update'),
+    path('services_delete/<int:pk>', Services_delete.as_view(), name='services_delete'),
+    path('parts_index/',Parts_index.as_view(),name='parts_index'),
 ]
