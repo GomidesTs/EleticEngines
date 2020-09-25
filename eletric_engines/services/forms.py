bea3fadcd8a9a5aa23c7b275690cb0588bbcf8da
GomidesTs <gomidestulio5@gmail.com>
2020-09-25 03:15:49 -0300

    Update forms services

--- eletric_engines/services/forms.py
+++ eletric_engines/services/forms.py
@@ -1,14 +1,17 @@
 from django import forms
 from .models import Services
 
-class Record(forms.Form):
+class Record(forms.ModelForm):
     class Meta:
         model = Services
-        filds = ['address', 'stret', 'number', 'zipe_code', 'reference']
+        fields = ['address', 'stret', 'number', 'zipe_code', 'reference', 'request_date']
 
-    
     address = forms.CharField(max_length=32, min_length=5, required=True)
     stret = forms.CharField(max_length=150, min_length=5, required=False)
-    number = forms.ImageField(max_length=5,required=False)
+    number = forms.IntegerField(required=False)
     zipe_code = forms.CharField(max_length=8, min_length=8,required=False)
     reference = forms.CharField(max_length=150,required=False)
+    request_date = forms.DateField(required=True)
+
+class Update(forms.ModelForm):
+    request_date = forms.DateField(required=True)
