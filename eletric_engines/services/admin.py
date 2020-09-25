45f502228e754b794358cdc49565e0ae6944690f
GomidesTs <gomidestulio5@gmail.com>
2020-09-25 03:13:21 -0300

    Implementing services admin to be displayed on the admin page

--- eletric_engines/services/admin.py
+++ eletric_engines/services/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Services
 
 # Register your models here.
+
+@admin.register(Services)
+class Services_admim(admin.ModelAdmin):
+    list_display = ['id', 'address', 'stret', 'number', 'zipe_code', 'reference']
